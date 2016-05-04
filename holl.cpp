9a76fd4abb3bf9a048dba5ee23a345799981b2e3
Sebastien <Sebastien@192.168.181.251>
2016-05-04 10:16:22 +0200

    Premier commit *save de holl

---
+++ holl.cpp
@@ -0,0 +1,18 @@
+/*
+ * holl.cpp
+ *
+ *  Created on: 2 mai 2016
+ *      Author: Sebastien
+ */
+
+#include "holl.h"
+
+holl::holl() {
+	// TODO Auto-generated constructor stub
+
+}
+
+holl::~holl() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ holl.h
@@ -0,0 +1,17 @@
+/*
+ * holl.h
+ *
+ *  Created on: 2 mai 2016
+ *      Author: Sebastien
+ */
+
+#ifndef HOLL_H_
+#define HOLL_H_
+
+class holl {
+public:
+	holl();
+	virtual ~holl();
+};
+
+#endif /* HOLL_H_ */
